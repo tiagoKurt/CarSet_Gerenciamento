62d0b1b854fcb5599023327beb48cea037c49f8e
Tiago Marques de Oliveira <lezzadogamer@gmail.com>
2023-06-07 13:17:42 -0300

    Update Proprietario.java

--- src/com/car/Modelos/Proprietario.java
+++ src/com/car/Modelos/Proprietario.java
@@ -5,17 +5,21 @@ import com.car.Enumerations.CategoriaCNH;
 
 public class Proprietario {
     private String CPF;
+    private String CNPJ;
     private String nome;
     private String telefone;
+    private String email;
     private String CNH;
     private CategoriaCNH categoriaCNH;
     
     public Proprietario(){}
     
-    public Proprietario(String CPF, String nome, String telefone, String CNH, CategoriaCNH categoriaCNH){
+    public Proprietario(String CPF, String CNPJ, String nome, String telefone, String email, String CNH, CategoriaCNH categoriaCNH){
         this.CPF = CPF;
+        this.CNPJ = CNPJ;
         this.nome = nome;
         this.telefone = telefone;
+        this.email = email;
         this.CNH = CNH;
         this.categoriaCNH = categoriaCNH;
     }
